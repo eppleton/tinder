59ac3ce5169c34230887a1c1663f5866c792102a
cloud <cloud@cloud-VirtualBox>
2019-02-27 14:08:25 +0100

    load profiles

--- client/src/main/java/de/eppleton/tinder/DataModel.java
+++ client/src/main/java/de/eppleton/tinder/DataModel.java
@@ -2,6 +2,7 @@ package de.eppleton.tinder;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import net.java.html.BrwsrCtx;
@@ -9,6 +10,7 @@ import net.java.html.json.ComputedProperty;
 import net.java.html.json.Function;
 import net.java.html.json.Model;
 import net.java.html.json.Models;
+import net.java.html.json.OnReceive;
 import net.java.html.json.Property;
 
 @Model(className = "Root", targetId = "",
@@ -19,6 +21,8 @@ import net.java.html.json.Property;
             , @Property(name = "nextProfile", type = Profile.class)
             , @Property(name = "newProfile", type = Profile.class)
             , @Property(name = "myProfile", type = Profile.class)
+            , @Property(name = "message", type = String.class)
+            , @Property(name = "baseUrl", type = String.class)
             , @Property(name = "loaded", type = boolean.class)
         })
 final class DataModel {
@@ -38,7 +42,8 @@ final class DataModel {
         } catch (IOException ex) {
             Logger.getLogger(DataModel.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        ui.setBaseUrl("http://localhost:8080/t/");
+        //	http://localhost:8080/t/java-app2/javafn-trigger
         ui.setActiveProfile(ui.getProfiles().get(0));
         ui.setNextProfile(ui.getProfiles().get(1));
         ui.setNewProfile(new Profile());
@@ -59,7 +64,30 @@ final class DataModel {
     
     @Function
     public static void storeProfile(Root ui){
-        ui.setMyProfile(ui.getNewProfile());
+        ui.storeProfileOnServer(ui.getBaseUrl()+"java-app2/javafn-trigger",ui.getNewProfile());
+    }
+    
+     @OnReceive(method = "POST", url = "{url}", data=Profile.class, onError = "cannotConnect")
+    public static void storeProfileOnServer(Root ui, Profile data){
+        ui.setMyProfile(data);
+        ui.loadProfiles(ui.getBaseUrl()+"getprofiles/getallprofiles-trigger");
+    }
+    
+     @OnReceive( url = "{url}", onError = "cannotConnect")
+    public static void getProfileFromServer(Root ui, Profile data){
+        ui.setMyProfile(data);
+        ui.setActiveProfile(data);
+    }
+    
+     @OnReceive(url = "{url}", onError = "cannotConnect")
+    static void loadProfiles(Root ui, List<Profile> arr) {
+        
+        ui.getProfiles().clear();     
+        ui.getProfiles().addAll(arr);
+    }
+    
+    static void cannotConnect(Root data, Exception ex) {
+        data.setMessage("Cannot connect " + ex.getMessage() + ". Should not you start the server project first?");
     }
 
 
